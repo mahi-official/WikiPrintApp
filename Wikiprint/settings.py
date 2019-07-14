9c450a88b5f12ad8dfbe4f75301492e2acf9939c
Mahesh <gmmgupta@gmail.com>
2019-07-14 15:04:33 +0530

    Update Index

--- Wikiprint/settings.py
+++ Wikiprint/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 STATIC_URL = '/static/'
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
